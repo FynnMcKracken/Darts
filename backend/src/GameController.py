64bd07f16d5f37418623e0a03f0cdc84cf9513e3
Fynn Theiß <fynn.theiss@web.de>
2021-02-15 10:48:25 +0100

    Refactored active player logic

--- backend/src/GameController.py
+++ backend/src/GameController.py
@@ -5,44 +5,41 @@ class GameState:
 
     def __init__(self):
         self.players = [
-            Player("Foo bo", 123, True),
-            Player("Bar bo", 8008, False),
-            Player("Baz bo", 47, False)
+            Player("Foo bo", 501),
+            Player("Bar bo", 501),
+            Player("Baz bo", 501)
         ]
-        self.current_player = 0
+        self.active_player = self.players[0]
         self.lastHit = None
 
     def register_new_player(self, player):
         self.players.append(player)
 
     def add_new_player(self, name):
-        player = Player(name, 0, False)
+        player = Player(name, 0)
         self.players.append(player)
 
     def next_player(self):
-        self.current_player = (self.current_player + 1) % len(self.players)
         for i in range(len(self.players)):
-            if self.players[i].active:
+            if self.players[i] == self.active_player:
                 print("Changing active player: Player", i)
-                self.players[i].active = False
-                self.players[(i + 1) % len(self.players)].active = True
+                self.active_player = self.players[(i + 1) % len(self.players)]
                 break
 
     def to_json(self):
         players_dict = {
             "lastHit": self.lastHit,
-            "players": [vars(x) for x in self.players]
+            "players": [{**vars(x), "active": self.active_player == x} for x in self.players]
         }
         return json.dumps(players_dict)
 
 
 class Player:
 
-    def __init__(self, name, score, active):
+    def __init__(self, name, score):
         self.name = name
         self.score = score
-        self.active = active
 
     def update_score(self, score):
-        self.score = self.score + score
+        self.score = score
 
