648b7bc31ebc2dfb37774171d0d2f0d37cbbe81e
Fynn Theiß <fynn.theiss@web.de>
2021-07-27 16:37:19 +0200

    Remapped controller hitCodes for different connection scheme

--- controller/DartsController/DartsController.ino
+++ controller/DartsController/DartsController.ino
@@ -12,16 +12,18 @@ const int outputPins[] = {10, 11, 12, 18, 19, 20, 21, 22, 23};
 const int outputPinCount = sizeof(outputPins) / sizeof(outputPins[0]);
 
 const char* const hitCodes[90] = {
-  "11x2", "14x2", "8x2", "16x2", "7x2", "19x2", "9x2", "12x2", "5x2", "20x2", 
-  "11i", "14i", "8i", "16i", "7i", "19i", "9i", "12i", "5i", "20i", 
-  "11x3", "14x3", "8x3", "16x3", "7x3", "19x3", "9x3", "12x3", "5x3", "20x3", 
-  "11o", "14o", "8o", "16o", "7o", "19o", "9o", "12o", "5o", "20o", 
+  "8i", "11i", "16i", "7i", "19i", "14i", "3i", "9i", "12i", "5i", 
+  "8x3", "11x3", "16x3", "7x3", "19x3", "14x3", "3x3", "9x3", "12x3", "5x3", 
+  "8x2", "11x2", "16x2", "7x2", "19x2", "14x2", "3x2", "9x2", "12x2", "5x2", 
+  "8o", "11o", "16o", "7o", "19o", "14o", "3o", "9o", "12o", "5o", 
   "_40", "_41", "_42", "_43", "_44", "_45", "_46", "_47", "Bullseye", "Bullseyex2", 
-  "10o", "6o", "15o", "2o", "17o", "3o", "13o", "4o", "18o", "1o", 
-  "10x2", "6x2", "15x2", "2x2", "17x2", "3x2", "13x2", "4x2", "18x2", "1x2", 
-  "10x3", "6x3", "15x3", "2x3", "17x3", "3x3", "13x3", "4x3", "18x3", "1x3", 
-  "10i", "6i", "15i", "2i", "17i", "3i", "13i", "4i", "18i", "1i",
+  "6o", "13o", "10o", "15o", "2o", "4o", "17o", "18o", "1o", "20o", 
+  "6x2", "13x2", "10x2", "15x2", "2x2", "4x2", "17x2", "18x2", "1x2", "20x2", 
+  "6x3", "13x3", "10x3", "15x3", "2x3", "4x3", "17x3", "18x3", "1x3", "20x3", 
+  "6i", "13i", "10i", "15i", "2i", "4i", "17i", "18i", "1i", "20i",
 };
+
+int timeout = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -64,7 +66,7 @@ void setup() {
   pinMode(23, OUTPUT);
   digitalWrite(23, HIGH);
 
-  Timer1.initialize(1000);
+  Timer1.initialize(128);
   Timer1.attachInterrupt(timer_isr);
 
 }
@@ -73,32 +75,38 @@ void timer_isr() {
   static int outputPinCounter = 0;
   static bool hits[inputPinCount * outputPinCount];
 
-  int outputPin = outputPins[outputPinCounter];
-  digitalWrite(outputPin , LOW);
-
-  for (int inputPinCounter = 0; inputPinCounter < inputPinCount; inputPinCounter++) {
-    int inputPin = inputPins[inputPinCounter];
-    int inputValue = digitalRead(inputPin);
-
-    bool &wasHitInLastCycle = hits[outputPinCounter * inputPinCount + inputPinCounter];
-    
-    if (inputValue == LOW && !wasHitInLastCycle) {
-      const char *hitCode = hitCodes[outputPinCounter * inputPinCount + inputPinCounter];
-      
-      Serial.print(hitCode);
-      Serial.print("\n");
-      
-      wasHitInLastCycle = true;
-    }
-
-    if (inputValue == HIGH && wasHitInLastCycle) {
-      wasHitInLastCycle = false;
-    }
-   
-  }
+  if (timeout == 0) {
   
-  digitalWrite(outputPin, HIGH);
-  outputPinCounter = (outputPinCounter + 1) % outputPinCount;
+    int outputPin = outputPins[outputPinCounter];
+    digitalWrite(outputPin , LOW);
+  
+    for (int inputPinCounter = 0; inputPinCounter < inputPinCount; inputPinCounter++) {
+      int inputPin = inputPins[inputPinCounter];
+      int inputValue = digitalRead(inputPin);
+  
+      bool &wasHitInLastCycle = hits[outputPinCounter * inputPinCount + inputPinCounter];
+      
+      if (inputValue == LOW && !wasHitInLastCycle) {
+        const char *hitCode = hitCodes[outputPinCounter * inputPinCount + inputPinCounter];
+        
+        Serial.print(hitCode);
+        Serial.print("\n");
+        
+        wasHitInLastCycle = true;
+        timeout = 150;
+      }
+  
+      if (inputValue == HIGH && wasHitInLastCycle) {
+        wasHitInLastCycle = false;
+      }
+     
+    }
+    
+    digitalWrite(outputPin, HIGH);
+    outputPinCounter = (outputPinCounter + 1) % outputPinCount;
+  } else {
+    timeout = timeout - 1;
+  }
   
 }
 
